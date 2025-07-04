b48c6ae7756181a4250cfde47dd1601c5760a418
Sridhar Sarnobat <ss401533@gmail.com>
2025-07-04 01:22:53 -0400

    2025-07-04: helloworld2.py

--- 11_grammar_parser_generator_lark/helloworld2.py
+++ 11_grammar_parser_generator_lark/helloworld2.py
@@ -9,3 +9,10 @@ start: WORD "," WORD "!"
 
 parser = Lark(grammar)
 
+def main():
+    print(parser.parse("Hello, world!"))
+    print(parser.parse("Adios, amigo!"))
+
+if __name__ == '__main__':
+    main()
+
