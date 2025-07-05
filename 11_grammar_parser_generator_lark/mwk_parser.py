e921a4acda21b834d20b9701fef54c469628efd2
Sridhar Sarnobat <ss401533@gmail.com>
2025-07-04 23:11:54 -0400

    2025-07-04: mwk_parser.py

--- 11_grammar_parser_generator_lark/mwk_parser.py
+++ 11_grammar_parser_generator_lark/mwk_parser.py
@@ -15,10 +15,12 @@ HEADING3:       /=== ===/
 HEADING2:       /== /
 DATESTAMP:      /[0-9]{4}-[0-9]{2}-[0-9]{2}/
 WHITESPACE:     /\s+/
-BODY.-1:        /(.|\s)+\s/
+LINE.-1:        /(.|\s)+/
 
-snippet:        HEADING3 NEWLINE BODY DATESTAMP  -> parse_snippet
-                | HEADING3   -> parse_snippet
+//HEADING3 NEWLINE LINE NEWLINE DATESTAMP NEWLINE HEADING3   -> parse_snippet
+
+snippet:        HEADING3 LINE+  -> parse_snippet
+//                | HEADING3   -> parse_snippet
 d:              DATESTAMP               -> parse_datestamp
 add_expr:       NUMBER "+" NUMBER       -> add_expr
 whitespace:     WHITESPACE              -> parse_whitespace
@@ -39,8 +41,8 @@ class CalcTransformer(Transformer):
     def parse_snippet(self, args):
         print("snippet(): " + args[0])
         print("snippet(): " + args[1])
-        print("snippet(): " + args[2])
-        print("snippet(): " + args[3])
+        # print("snippet(): " + args[2])
+        # print("snippet(): " + args[3])
         return args[0]
 
     def parse_datestamp(self, args):
@@ -54,13 +56,24 @@ class CalcTransformer(Transformer):
         return int(args[0]) - int(args[1])
 
 
-parser = Lark(grammar, parser='lalr', lexer="contextual",
-    transformer=CalcTransformer())
+
+# parser = Lark(grammar, lexer="dynamic_complete")
+parser =  Lark(grammar, parser="earley", lexer="dynamic_complete")
+    # transformer=CalcTransformer())
+# parser = Lark(grammar, parser='lalr', lexer="contextual",
+# transformer=CalcTransformer()
+# result = transformer.transform(tree)
+# print(result)
+
 
 def main():
     text = sys.stdin.read().strip()
 
     tree = parser.parse(text)
+
+    result = CalcTransformer().transform(tree)
+
+    print(result)
 
 if __name__ == '__main__':
     main()
