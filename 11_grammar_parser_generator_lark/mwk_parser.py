9b04a3534c33e8668f63e6d93b58dc91222f0d22
Sridhar Sarnobat <ss401533@gmail.com>
2025-07-04 23:30:39 -0400

    2025-07-04: mwk_parser.py

--- 11_grammar_parser_generator_lark/mwk_parser.py
+++ 11_grammar_parser_generator_lark/mwk_parser.py
@@ -17,7 +17,7 @@ DATESTAMP:      /[0-9]{4}-[0-9]{2}-[0-9]{2}/
 WHITESPACE:     /\s+/
 BODY:           /(.|\s)+/
 
-snippet:        HEADING3 NEWLINE BODY NEWLINE DATESTAMP NEWLINE \
+snippet:        HEADING3 NEWLINE HASHTAGS BODY NEWLINE DATESTAMP NEWLINE \
                                         -> parse_snippet
                 | HEADING3              -> parse_ending
 add_expr:       NUMBER "+" NUMBER       -> add_expr
@@ -27,20 +27,20 @@ whitespace:     WHITESPACE              -> parse_whitespace
 
 class CalcTransformer(Transformer):
 
-    def parse_whitespace(self, args):
+    def parse_whitespace    (self, args):
         # print("parse_whitespace(): " + str(type(args[0])))
-        print("parse_whitespace(): " + args[0], end="")
+        print("parse_whitespace(): "    + args[0], end="")
         return args[0]
         
-    def parse_unhandled(self, args):
-        print("parse_unhandled(): " + args[0])
+    def parse_unhandled     (self, args):
+        print("parse_unhandled(): "     + args[0])
         return args[0]
 
-    def parse_ending(self, args):
-        print("parse_ending(): " + args[0])
+    def parse_ending        (self, args):
+        print("parse_ending(): "        + args[0])
         return args[0]
 
-    def parse_snippet(self, args):
+    def parse_snippet        (self, args):
         print("snippet(): heading "     + args[0])
         print("snippet(): "             + args[1], end="")
         print("snippet(): "             + args[2])
@@ -48,22 +48,10 @@ class CalcTransformer(Transformer):
         print("snippet(): datestamp = " + args[4])
         return args[0]
 
-    def add_expr(self, args):
+    def add_expr            (self, args):
         return int(args[0]) + int(args[1])
 
-    def sub_expr(self, args):
-        return int(args[0]) - int(args[1])
-
-
-
-# parser = Lark(grammar, lexer="dynamic_complete")
 parser =  Lark(grammar, parser="earley", lexer="dynamic_complete")
-    # transformer=CalcTransformer())
-# parser = Lark(grammar, parser='lalr', lexer="contextual",
-# transformer=CalcTransformer()
-# result = transformer.transform(tree)
-# print(result)
-
 
 def main():
     text = sys.stdin.read().strip()
@@ -71,8 +59,6 @@ def main():
     tree = parser.parse(text)
 
     result = CalcTransformer().transform(tree)
-
-    #print(result)
 
 if __name__ == '__main__':
     main()
