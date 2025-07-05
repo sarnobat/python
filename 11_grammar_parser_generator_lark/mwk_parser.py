020fd24ea9362a5a215ed163e312e7b18006aa77
Sridhar Sarnobat <ss401533@gmail.com>
2025-07-04 21:29:49 -0400

    2025-07-04: mwk_parser.py

--- 11_grammar_parser_generator_lark/mwk_parser.py
+++ 11_grammar_parser_generator_lark/mwk_parser.py
@@ -10,29 +10,33 @@ start: line+
 %import common.NUMBER
 %import common.NEWLINE
 
-%ignore NEWLINE
-%ignore WS_INLINE
 
 line:   d
     | snippet
+    | whitespace
     | unhandled
 
 
 HEADING3:       /=== ===/
 HEADING2:       /== /
 DATESTAMP:      /[0-9]{4}-[0-9]{2}-[0-9]{2}/
+WHITESPACE:     /\s+/
 WILDCARD:       /.+/
 
+whitespace:     WHITESPACE              -> parse_whitespace
 snippet:        HEADING3                -> parse_snippet
 d:              DATESTAMP               -> parse_datestamp
 add_expr:       NUMBER "+" NUMBER       -> add_expr
 unhandled:      WILDCARD                -> parse_unhandled
 
-
 """
 
 class CalcTransformer(Transformer):
 
+    def parse_whitespace(self, args):
+        print("parse_whitespace(): " + args[0])
+        return args[0]
+        
     def parse_unhandled(self, args):
         print("parse_unhandled(): " + args[0])
         return args[0]
