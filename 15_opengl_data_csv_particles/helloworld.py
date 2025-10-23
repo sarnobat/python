b03168a9ba197cd87c9ef8ab8c487e4e6f81a942
Sridhar Sarnobat <ss401533@gmail.com>
2025-10-22 22:22:16 -0400

    2025-10-22: helloworld.py

--- 15_opengl_data_csv_particles/helloworld.py
+++ 15_opengl_data_csv_particles/helloworld.py
@@ -3,29 +3,75 @@
 
 import sys
 import pandas as pd
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import glfw
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 if len(sys.argv) < 2:
     print(f"Usage: {sys.argv[0]} <csv_file>")
     sys.exit(1)
 
 csv_file = sys.argv[1]
-
-# Load CSV
 data = pd.read_csv(csv_file)
 
-# Create 3D figure
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+angle = 0.0
 
-# Scatter plot
-ax.scatter(data['x'], data['y'], data['z'], c='red', s=50)
+def main():
+    global angle
 
-# Labels
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_title('3D Particle Plot')
+    if not glfw.init():
+        print("Failed to initialize GLFW")
+        sys.exit(1)
 
-plt.show()
+    window = glfw.create_window(800, 600, "Rotating 3D Particles", None, None)
+    if not window:
+        glfw.terminate()
+        print("Failed to create GLFW window")
+        sys.exit(1)
+
+    glfw.make_context_current(window)
+    glEnable(GL_DEPTH_TEST)
+    glPointSize(5.0)
+    glClearColor(0.1, 0.1, 0.1, 1.0)
+
+    # Perspective projection
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, 800/600, 1, 1000)
+    glMatrixMode(GL_MODELVIEW)
+
+    # Compute scale to center particles
+    x_min, x_max = data['x'].min(), data['x'].max()
+    y_min, y_max = data['y'].min(), data['y'].max()
+    z_min, z_max = data['z'].min(), data['z'].max()
+    x_center = (x_min + x_max)/2
+    y_center = (y_min + y_max)/2
+    z_center = (z_min + z_max)/2
+
+    while not glfw.window_should_close(window):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        gluLookAt(0, 0, 200, 0, 0, 0, 0, 1, 0)
+
+        # Rotate around Y-axis
+        glTranslatef(-x_center, -y_center, -z_center)
+        glRotatef(angle, 0, 1, 0)
+        glTranslatef(x_center, y_center, z_center)
+        angle += 0.5
+        if angle >= 360.0:
+            angle -= 360.0
+
+        # Draw particles
+        glBegin(GL_POINTS)
+        for _, row in data.iterrows():
+            glColor3f(0.0, 0.6, 1.0)
+            glVertex3f(row['x'], row['y'], row['z'])
+        glEnd()
+
+        glfw.swap_buffers(window)
+        glfw.poll_events()
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
