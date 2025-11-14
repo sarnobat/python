017d26b4e8380e4108781dc94f7ab1129f9cd070
Sridhar Sarnobat <ss401533@gmail.com>
2025-11-13 21:38:40 -0500

    2025-11-13: Makefile hello.cpp libhello.dylib test.py

--- 19_python_c++_call/hello.cpp
+++ 19_python_c++_call/hello.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
 extern "C" void greet(const char* name) {
+    std::fprintf(stderr, "[trace] %10s:%-5d %32s() SRIDHAR\n", __FILE__, __LINE__, __func__);
     std::cout << "Hello, " << name << " from C++" << std::endl;
 }
--- 19_python_c++_call/test.py
+++ 19_python_c++_call/test.py
@@ -1,9 +1,10 @@
 import ctypes
 from pathlib import Path
 
+
 lib_path = Path(__file__).parent / "libhello.dylib"
 lib = ctypes.CDLL(str(lib_path))
-
 lib.greet.argtypes = [ctypes.c_char_p]
 
+print("[trace]	test.py  ")
 lib.greet(b"Sridhar")
