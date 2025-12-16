202aad603375f605a8da0391850dd128aacecf41
Sridhar Sarnobat <ss401533@gmail.com>
2025-12-16 01:00:59 -0500

    2025-12-15: mwk_parser.py

--- 11_grammar_parser_generator_lark/mwk_parser.py
+++ 11_grammar_parser_generator_lark/mwk_parser.py
@@ -1,5 +1,5 @@
 # cat ~/mwk.git/apple_notes_read_only/main_iphone.mwk.becomesempty | head -100 | python3 /Volumes/git/github/python/11_grammar_parser_generator_lark/mwk_parser.py
-from lark import Lark, Transformer
+from lark import Lark, Transformer, Token, Tree
 import sys
 
 grammar = r"""
@@ -16,7 +16,7 @@ HASHTAG: /#[^\n]*/ NEWLINE
 DATESTAMP:      /[0-9]{4}-[0-9]{2}-[0-9]{2}/
 WHITESPACE:     /\s+/
 # BODY1:           /(.|\s)+?(?=(=== ===|\d{4}-\d{2}-\d{2}|\Z))/
-BODY1: /(.|\s)+?(?=(===[^\r\n]*===|\#[^\n]*\n|\d{4}-\d{2}-\d{2}|\Z))/
+BODY1: /(?!#)(.|\s)+?(?=(===[^\r\n]*===|\#[^\n]*\n|\d{4}-\d{2}-\d{2}|\Z))/
 # BODY1: /(?!#)(?!#)(.|\s)+?(?=(===[^\r\n]*===|\#[^\n]*\n|\d{4}-\d{2}-\d{2}|\Z))/
 # BODY_LINE: /(?!===)(?!\d{4}-\d{2}-\d{2})[^\n]+/
 
@@ -24,9 +24,9 @@ BODY1: /(.|\s)+?(?=(===[^\r\n]*===|\#[^\n]*\n|\d{4}-\d{2}-\d{2}|\Z))/
 body: BODY1 -> parse_body
 # body: (BODY_LINE NEWLINE*)+
 hashtags:       HASHTAG*     -> parse_hashtags
-snippet:        HEADING3 NEWLINE* hashtags body NEWLINE DATESTAMP NEWLINE \
+snippet:        HEADING3 NEWLINE* hashtags? body? NEWLINE? DATESTAMP? NEWLINE \
                                         -> parse_snippet
-                | HEADING3 body              -> parse_ending
+                | HEADING3 NEWLINE* body? NEWLINE*         -> parse_ending
 
 unparseable:    body -> parse_unparseable
 """
@@ -53,7 +53,11 @@ class MwkTransformer(Transformer):
         # args = [text, newline]
         # return args[0] + args[1]
         return args.value.strip()
-    
+
+
+    # def NEWLINE(self, tok: Token) -> None:
+    #     return None   # remove NEWLINE entirely
+
     # def BODY1(self, args):
     #     print("BODY1(): " + args.value, end="")
     #     return args.value.strip()
@@ -76,7 +80,7 @@ class MwkTransformer(Transformer):
         print("snippet(): heading "     + args[0])
         print("snippet(): newline "             + args[1], end="")
         print("snippet(): hashtag: "             + str(args[2]), end="\n")
-        print("snippet(): "             + str(args[3]), end="")
+        print("snippet(): body:"             + args[3], end="")
         print("snippet():   " + args[5])
         print("snippet(): datestamp = " + args[4])
         return args[0]
